b8f863ed55dec1c570495959cb65cf2bf3f08f80
alaa <alaamohammedsaif@gmail.com>
2021-01-28 00:32:05 +0200

    Actions Added

---
+++ src/actions/index.js
@@ -0,0 +1,69 @@
+const URL = "http://localhost:3001/students";
+export const getAllStudent = async () => {
+    let payload;
+    try {
+        let response = await fetch(`${URL}`);
+        payload = await response.json()
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+    return {
+        type: 'STUDENTS_LIST',
+        payload
+    }
+
+}
+export const getStudentDetails = async (id) => {
+    let payload;
+    try {
+        let response = await fetch(`${URL}/${id}`);
+        payload = await response.json()
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+    return {
+        type: 'STUDENT_DETAILS',
+        payload
+    }
+}
+export const clearStudentDetails = () => {
+    return {
+        type: 'CLEAR_STUDENT_DETAILS',
+        payload: null
+    }
+}
+
+export const addStudent = async (student) => {
+    let payload = student;
+    try {
+        axios.post(`${URL}`, {student})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+    return {
+        type: 'ADD_STUDENT',
+        payload
+    }
+}
+export const deleteStudent = (id)=>{
+    let payload =null;
+    axios.delete(`${URL}/${id}`)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    return {
+        type: 'DELETE_STUDENT',
+        payload
+    }
+}
